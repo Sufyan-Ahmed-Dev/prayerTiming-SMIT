24f5b37ebdb9104207f08b2e913b3aec45214265
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-04-21 22:38:11 +0500

    approximately Done

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 let inputForm = document.querySelector("#tableForm");
 let input = document.querySelector("#default-search");
-let CityName = document.querySelector("#locationShow");
+let tableRow = document.querySelector("#rows");
 
-inputForm.addEventListener("submit" , (e)=>{
-    e.preventDefault()
-    let getCityName = input.value ;
-    console.log(getCityName)
-    CityName.innerHTML += getCityName
-    
-})
+// let tableRows ;
+inputForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  let getCityName = input.value;
+  // console.log(getCityName)
+  var currentDate = new Date();
+  // const formattedDate = currentDate.toISOString().split('T')[0];
+
+  try {
+    const res = await axios.get(
+      `https://api.aladhan.com/v1/timingsByAddress/${currentDate}?address=${getCityName}`
+    );
+    let getData = res.data.data.timings;
+    tableRow.innerHTML = " ";
+
+    Object.entries(getData).forEach(([key, value]) => {
+      console.log(`${key}: ${value}`);
+      tableRow.innerHTML += ` <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+        ${key}
+        </th>
+        <td class="px-6 py-4">
+        ${value}
+        </td>
+       
+    </tr>`;
+    });
+
+    console.log(getData);
+  } catch (err) {
+    console.log(err);
+  }
+  // https://muslimsalat.com/lahore.json
+});
+
+// console.log(axios)
